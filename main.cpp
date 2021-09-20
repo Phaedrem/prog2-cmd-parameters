ec67ab19f38867f91042f0cff681bf72da85236c
Darren Bowers <darren.bowers@g.austincc.edu>
2021-09-20 13:42:15 -0500

    added ellipsis to cout on line 10 to match example

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ Purpose: Program to analyze and report back command line parameters.
 #include "main.h"
 
 int main(int argc, char** argv){
-    cout << "My Command line analyzer" << endl;
+    cout << "My Command line analyzer..." << endl;
     cout << "The number of command line parameters are: " << argc <<endl;
     cout << "The length of the name of the program is: " << string_length(argv[0]) << std::endl;
     if(argc >1){
