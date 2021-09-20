c9503be89f86071bc8c2b1bdd9c9da6912ea2fa5
Darren Bowers <darren.bowers@g.austincc.edu>
2021-09-20 13:40:20 -0500

    removed else statement, added for loop to match length of argc and report back the length of each parameter passed

--- main.cpp
+++ main.cpp
@@ -11,11 +11,9 @@ int main(int argc, char** argv){
     cout << "The number of command line parameters are: " << argc <<endl;
     cout << "The length of the name of the program is: " << string_length(argv[0]) << std::endl;
     if(argc >1){
-        std::cout << "The length of the first parameter is " << string_length(argv[1]) << std::endl;
-    } else {
-        cout << "There are no command line parameters after the program name." << endl;
+        for(int i=1; i<argc; i++){
+        std::cout << "The length of the parameter " << i << " is " << string_length(argv[i]) << std::endl;
+        }
     }
-
-
     return 0;
 }
