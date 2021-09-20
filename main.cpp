42a3df0450845cc64c58f8e0cdd93ebbd1e2825c
Darren Bowers <darren.bowers@g.austincc.edu>
2021-09-20 12:04:30 -0500

    changed provided text to reflect programs purpose and added new line to report argc results

--- main.cpp
+++ main.cpp
@@ -7,8 +7,9 @@ Purpose: Program to analyze and report back command line parameters.
 #include "main.h"
 
 int main(int argc, char** argv) {
-    cout << "Hello World" << endl;
-
+    cout << "My Command line analyzer" << endl;
+    cout << "The number of command line parameters are: " << argc <<endl;
+    
 
     return 0;
 }
