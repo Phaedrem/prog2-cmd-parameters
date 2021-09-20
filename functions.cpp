176a5fea0d6229a0ffe78dc2006dcd3dd055abe9
Darren Bowers <darren.bowers@g.austincc.edu>
2021-09-20 12:28:35 -0500

    stubbed and prototyped string_length function

--- functions.cpp
+++ functions.cpp
@@ -3,3 +3,11 @@ Name: Darren Bowers
 Coding 02
 Purpose: Program to analyze and report back command line parameters. 
 **********************/
+
+#include "functions.h"
+
+int string_length(const char *str){
+    
+    return 0;
+}
+
--- functions.h
+++ functions.h
@@ -7,8 +7,7 @@ Purpose: Program to analyze and report back command line parameters.
 #ifndef COMMANDLINE_FUNCTIONS_H
 #define COMMANDLINE_FUNCTIONS_H
 
-
-
+int string_length(const char*);
 
 #endif //COMMANDLINE_FUNCTIONS_H
 
