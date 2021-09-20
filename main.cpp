a30e7ff176d504accc813cbb929d7ba9306961b5
Darren Bowers <darren.bowers@g.austincc.edu>
2021-09-20 18:43:44 -0500

    removed redundant if statement

--- main.cpp
+++ main.cpp
@@ -10,10 +10,8 @@ int main(int argc, char** argv){
     cout << "My Command line analyzer..." << endl;
     cout << "The number of command line parameters are: " << argc <<endl;
     cout << "The length of the name of the program is: " << string_length(argv[0]) << std::endl;
-    if(argc >1){
-        for(int i=1; i<argc; i++){
-        std::cout << "The length of the parameter " << i << " is " << string_length(argv[i]) << std::endl;
-        }
+    for(int i=1; i<argc; i++){
+    std::cout << "The length of the parameter " << i << " is " << string_length(argv[i]) << std::endl;
     }
     return 0;
 }
