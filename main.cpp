f6af14c2835baed68dad03ca2f4c4b9e4b3430c9
Darren Bowers <darren.bowers@g.austincc.edu>
2021-09-20 13:25:32 -0500

    added cout to report length of the program name

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ Purpose: Program to analyze and report back command line parameters.
 int main(int argc, char** argv){
     cout << "My Command line analyzer" << endl;
     cout << "The number of command line parameters are: " << argc <<endl;
+    cout << "The length of the name of the program is: " << string_length(argv[0]) << std::endl;
     if(argc >1){
         std::cout << "The length of the first parameter is " << string_length(argv[1]) << std::endl;
     } else {
