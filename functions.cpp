d510ce0c041cbfa21082a0709d5fa2a7889fb7cf
Darren Bowers <darren.bowers@g.austincc.edu>
2021-09-20 13:17:12 -0500

    added else statement to main.cpp for testing parameters, added while loop to functions.cpp to count paramater length

--- functions.cpp
+++ functions.cpp
@@ -7,7 +7,10 @@ Purpose: Program to analyze and report back command line parameters.
 #include "functions.h"
 
 int string_length(const char *str){
-    
-    return 0;
+    int count = 0;
+    while(str[count] != '\0'){
+        count++;
+    }
+    return count;
 }
 
--- main.cpp
+++ main.cpp
@@ -11,7 +11,10 @@ int main(int argc, char** argv){
     cout << "The number of command line parameters are: " << argc <<endl;
     if(argc >1){
         std::cout << "The length of the first parameter is " << string_length(argv[1]) << std::endl;
+    } else {
+        cout << "There are no command line parameters after the program name." << endl;
     }
+
 
     return 0;
 }
